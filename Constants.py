e65b076d0d4bd6a9efe82fb47184aa8b92ff509e
David Pujol <david.pujol@decisionbrain.com>
2024-09-09 11:41:42 +0200

    Adjust delay

--- Constants.py
+++ Constants.py
@@ -91,7 +91,7 @@ WILD_WALKING_DIRECTION = 'EW'
 MOVE_FORWARD_STATIC_ENCOUNTER = False
 SKIP_UPDATING_GAME = False
 # Some static encounters make a white screen flash before entering the combat
-# Raise this value to 5 for Dialga and Palkia; default value is 2
+# Raise this value to 4 for Dialga and Palkia; default value is 2
 STATIC_ENCOUNTERS_DELAY = 2
 # How long has the bot been stuck in the same state before restarting the game
 STUCK_TIMER_SECONDS = 30
