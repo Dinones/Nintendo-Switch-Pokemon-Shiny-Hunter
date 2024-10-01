495b514aff76449d71a79577e8d83c6e4cd2a82a
David Pujol <david.pujol@decisionbrain.com>
2024-10-01 11:39:22 +0200

    Fix restart_game_macro that can lead to sleep mode

--- Modules/Macros.py
+++ Modules/Macros.py
@@ -86,6 +86,12 @@ def restart_game_macro(controller):
     controller.current_button_pressed = 'HOME'
     controller.nxbt_manager.press_buttons(controller.controller_index, [Buttons.HOME], down=0.05, up=0)
     controller.current_button_pressed = ''
+
+    controller.current_button_pressed = 'B'
+    controller.nxbt_manager.press_buttons(controller.controller_index, [Buttons.B])
+    controller.current_button_pressed = ''
+    sleep(0.2)
+
     sleep(1.5); controller.current_button_pressed = 'X'
     controller.nxbt_manager.press_buttons(controller.controller_index, [Buttons.X]); sleep(0.5)
     if CONST.SKIP_UPDATING_GAME:
